1e1bf91dd3262546c235014b975f69e648d8a546
Simon Kassel <kassel.simon@gmail.com>
2017-09-13 11:50:10 -0400

    add wide format vars

--- R/combine.R
+++ R/combine.R
@@ -13,8 +13,12 @@
 
 library(tidyverse)
 library(sf)
+library(stringr)
 
+# helper functions
 source("R/functions.R")
+
+# NTD tabular data --------------------------------------------------------
 
 # ntd descriptor vars
 exp <- read.csv("data/ntd/expenses/output/expense_vars.csv", 
@@ -36,24 +40,137 @@ lookup <- read.csv("data/spatial/output/name_lookup.csv",
 ntd_vars <- left_join(exp, svc) %>%
   left_join(rev) %>%
   right_join(lookup, .) %>%
-  select(-starts_with("X"))
-
-ntd_vars <- ntd_vars %>%
+  select(-starts_with("X")) %>%
   mutate(average_speed = revenue_miles / revenue_hours,
          avg_fare = fares / upt,
          farebox_recovery = fares / total_expenses)
 
-write.csv(ntd_vars, "data/r_output/ntd_variables.csv")
 
 # spatial data ------------------------------------------------------------
 
+# load spatial data
 load("data/spatial/output/spatial_data.Rdata")
 
-ag_with_geo <- ag_with_geo %>%
-  mutate(ntdid = clean_ntdid(ntdid))
+# join ntd_vars to geographic agency data
+ag_long <- right_join(ag_with_geo, ntd_vars)
 
-ag_with_ntd <- right_join(ag_sf, ntd_vars)
+# create wide format dataset
+ag_wide <- select(
+  ag_long, ntdid, GEOID.msa, GEOID.tract, name, city, state, zip) %>%
+  unique()
+for(y in unique(ag_long$year)) {
+  temp <- ag_long %>%
+    filter(year == y) %>%
+    as.data.frame %>%
+    select(ntdid, total_expenses:farebox_recovery)
+  names(temp)[2:ncol(temp)] <- paste("y", y, names(temp)[2:ncol(temp)], sep = ".")
+  ag_wide <- left_join(ag_wide, temp)
+} 
 
-st_write(ag_with_ntd, "data/r_output/agencies_with_ntd_variables.geojson")
 
+# census data -------------------------------------------------------------
+
+read_census <- function(file, dir, geog) {
+  
+  table <- paste0(dir, file) %>%
+    read.csv(stringsAsFactors = FALSE)
+  
+  if ("X" %in% names(table)) {
+    table <- table %>%
+      select(-X)
+  }
+  
+  if (tolower(geog) == "msa") {
+    table <- table %>%
+      rename(GEOID.msa = metropolitan.statistical.area.micropolitan.statistical.area,
+             name.msa = NAME)
+  } else if (tolower(geog) == "tract") {
+    table <- table %>%
+      rename(GEOID.tract = tract) %>%
+      select(-NAME, -state, -county)
+  } else {
+    paste0(geog, " is not a valid geography, please choose either 'msa' or 'tract' ")
+    return(NULL)
+  } 
+  
+  year <- substr(file, nchar(file) - 7, nchar(file) - 4)
+  
+  table <- table %>% 
+    mutate(year = year)
+
+  return(table)
+}
+
+widen <- function(dat, prefix_col, join_col, remove_cols) {
+  
+  iter <- dat %>%
+    select(starts_with(prefix_col))
+  names(iter) <- "subvar"
+  
+  dat <- cbind(dat, iter) 
+  
+  iter <- iter[,1] %>%
+    unique
+  
+  init_df <- dat %>%
+    select(one_of(join_col)) %>%
+    unique()
+  
+  for (i in iter) {
+    temp <- dat %>%
+      filter(subvar == i) %>%
+      select(-one_of(c(prefix_col, "subvar", remove_cols)))
+    names(temp)[-1] <- paste0("y.", i, ".", names(temp)[-1])
+    init_df <- left_join(init_df, temp, by = join_col)
+  }
+  
+  return(init_df)
+  
+}
+
+census_files <- dir("data/census/output/")
+
+cen_met <- census_files[grepl("^metro", census_files)]
+cen_trc <- setdiff(census_files, cen_met)
+
+# get metropolitan long
+cen_met_long <- map_dfr(
+  cen_met, 
+  read_census, 
+  dir = "data/census/output/", geog="msa")
+
+# get metropolitan wide
+cen_met_wide <- widen(
+  dat = cen_met_long, 
+  prefix_col = "year", 
+  join_col = "GEOID.msa", 
+  remove_cols = "name.msa")
+
+# get tract long
+cen_tract_long <- map_dfr(
+  cen_trc, 
+  read_census, 
+  dir = "data/census/output/", geog="tract")
+
+# get metropolitan wide
+# cen_tract_wide <- widen(
+#   dat = cen_tract_long, 
+#   prefix_col = "year", 
+#   join_col = "GEOID.tract", 
+#   remove_cols = "year")
+
+# export datasets ---------------------------------------------------------
+
+# write long form agency datasets
+st_write(ag_long, "data/r_output/agencies_with_ntd_variables_long.geojson")
+write.csv(ag_long, "data/r_output/agencies_with_ntd_variables_long.csv")
+
+# write wide form agency datasets
+st_write(ag_wide, "data/r_output/agencies_with_ntd_variables_wide.geojson")
+write.csv(ag_wide, "data/r_output/agencies_with_ntd_variables_wide.csv")
+
+# census datasets
+write.csv(cen_met_long, "data/r_output/census_vars_metro_areas_long.csv")
+write.csv(cen_met_wide, "data/r_output/census_vars_metro_areas_wide.csv")
+write.csv(cen_tract_long, "data/r_output/census_vars_tracts_long.csv")
 

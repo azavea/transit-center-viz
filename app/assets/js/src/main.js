2ff7ccd16ce6d1e241e16ef4997464b8fe771cba
eneedham <eneedham@design.upenn.edu>
2017-10-25 15:14:46 -0400

    updated zoom variales to zoomThreshold, created tcviz-config file

--- app/assets/js/src/main.js
+++ app/assets/js/src/main.js
@@ -12,10 +12,6 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     ext: 'png'
 }).addTo(map);
 
-//Zoom thresholds for nationwide layers versus MSA layers
-var zoomNationwide = 9;
-var zoomMSA = 10;
-
 //function to change layer selector options when zoom level changes
 var changeToggle = function (select, layer_options){
     select.clear();
@@ -47,11 +43,10 @@ $(document).ready(function () {
 
     // change layer selector options based on zoom level of the map
     map.on('zoomend', function () {
-        // console.log(map.getZoom());
-        if (map.getZoom() >= zoomMSA) {
+        if (map.getZoom() > TCVIZ.Config.zoomThreshold) {
             changeToggle(layerToggle, TCVIZ.Config.MSA_layers);
         }
-        if (map.getZoom() <= zoomNationwide) {
+        if (map.getZoom() <= TCVIZ.Config.zoomThreshold) {
             changeToggle(layerToggle, TCVIZ.Config.nationwide_layers);
         }
     });
---
+++ app/assets/js/src/tcviz-config.js
@@ -0,0 +1,112 @@
+TCVIZ.Config = {
+    zoomThreshold: 9,
+    nationwide_layers: [
+        {
+            text: 'Total Ridership',
+            value: 1,
+            id: 'totalRide'
+        },
+        {
+            text: 'Bus Ridership',
+            value: 2,
+            id: 'busRide'
+        },
+        {
+            text: 'Rail Ridership',
+            value: 3,
+            id: 'railRide'
+        },
+        {
+            text: 'Average Fare',
+            value: 4,
+            id: 'avgFare'
+        },
+        {
+            text: 'Average Speed',
+            value: 5,
+            id: 'avgSpeed'
+        },
+        {
+            text: 'Farebox Recovery',
+            value: 6,
+            id: 'fareBox'
+        },
+        {
+            text: 'Vehicle Revenue Miles',
+            value: 7,
+            id: 'revMiles'
+        },
+        {
+            text: 'Expenses',
+            value: 8,
+            id: 'expenses'
+        },
+        {
+            text: 'Statewide Gas Prices',
+            value: 9,
+            id: 'gas'
+        }
+    ],
+    MSA_layers: [
+        {
+            text: 'Percent Commute by Driving',
+            value: 1,
+            id: 'driving'
+        },
+        {
+            text: 'Percent Commute by Public Transit',
+            value: 2,
+            id: 'transit'
+        },
+        {
+            text: 'Total Employment',
+            value: 3,
+            id: 'employment'
+        },
+        {
+            text: 'Households with No Vehicle',
+            value: 4,
+            id: 'vehicle',
+        },
+        {
+            text: 'Median Household Income',
+            value: 5,
+            id: 'medianHH'
+        },
+        {
+            text: 'Households Below 200% of Poverty Line',
+            value: 6,
+            id: 'poverty'
+        },
+        {
+            text: 'Population Density',
+            value: 7,
+            id: 'popDensity'
+        },
+        {
+            text: 'Total Population Foregin Born',
+            value: 8,
+            id: 'foreignBorn'
+        },
+        {
+            text: 'Percent Black',
+            value: 9,
+            id: 'pctBlack'
+        },
+        {
+            text: 'Percent Asian',
+            value: 10,
+            id: 'pctAsian'
+        },
+        {
+            text: 'Percent White',
+            value: 11,
+            id: 'pctWhite'
+        },
+        {
+            text: 'Percent Hispanic/Latino of any race',
+            value: 12,
+            id: 'pctHisp'
+        }
+    ]
+};

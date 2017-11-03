088b02033cf0f411a920aca8958c2f36cb22a406
Simon Kassel <kassel.simon@gmail.com>
2017-11-03 18:01:56 -0400

    refactor code according to Andrew's feedback

--- app/assets/js/src/main.js
+++ app/assets/js/src/main.js
@@ -1,5 +1,6 @@
 /* global L */
 
+// TODO: should these be in a config file?
 var map = L.map('map', {
     center: [39.500, -98.35],
     zoom: 4
@@ -14,124 +15,47 @@ L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
 
 $(document).ready(function() {
 
-    /**
-     * Set a nationwide point layer
+    /*
+     Initialize objects
      */
-    function setNationalGeoJSONLayer() {
-        var valueField = $('#toggle')[0].selectize.items[0];
-        if (valueField !== undefined) {
-            if (map.hasLayer(TCVIZ.Config.featureGroup)) {
-                map.removeLayer(TCVIZ.Config.featureGroup);
-            }
-            TCVIZ.Config.ntdField = valueField;
-            TCVIZ.Config.sqlConnections.msaMap.getJson(valueField)
-                .done(function(data) {
-                    data = removeNAs(data, valueField);
-                    TCVIZ.Config.featureGroup = L.geoJson(data, {
-                        pointToLayer: function(feature, latlng) {
-                            return new L.CircleMarker(latlng, {
-                                // * TODO: function to scale values
-                                // * TODO: timeseries stylesheet interferes with
-                                // *  graduated symbols, fix this
-                                radius: feature.properties[valueField] / 1000000,
-                                fillOpacity: 0.5,
-                                color: '#0000FF'
-                            });
-                        }
-                    });
-                    map.addLayer(TCVIZ.Config.featureGroup);
-                });
-        }
-    }
+    // Carto connections
+    TCVIZ.Connections.msaMap = new TCVIZ.Carto.SQL('msaMap');
+    TCVIZ.Connections.timeSeriesSql = new TCVIZ.Carto.SQL('timeSeries');
+    TCVIZ.Connections.tractMap = new TCVIZ.Carto.SQL('censusTracts');
 
-    /**
-     * Set polygon layer of MSAs
+    // Time Series plot
+    TCVIZ.State.timeseries = new TCVIZ.Charts.TimeSeries();
+
+    /*
+    Initialize
      */
-    function setMSAGeoJSONLayer() {
-        var valueField = layerToggleValue();
-        if (valueField !== undefined) {
-            if (map.hasLayer(TCVIZ.Config.featureGroup)) {
-                map.removeLayer(TCVIZ.Config.featureGroup);
-            }
-            TCVIZ.Config.censusField = valueField;
-            // TCVIZ.Config.sqlConnections.tractMap.getJson()
-            //     .done(function(data) {
-            // // TODO: add the rest of this function once census data are up
-            //     });
-        }
-    }
+    initialize();
+
+    /*
+    Listeners
+     */
+    $('#toggle')[0].selectize.on('change', function() {
+        setMap();
+        setTimeSeries();
+    });
+    $('#MSA_toggle')[0].selectize.on('change', setTimeSeries);
+    map.on('zoomstart', onZoomChange);
+
+    /*
+    Functions--------------------------------------------------------------
+     */
 
     /**
-     * Send a layer to the map calling function based on map zoom
+     * Change layer selector options when zoom level changes
+     * @param  {Selector}
+     * @param  {Array}
      */
-    function setMap() {
-        if (isNationWide()) {
-            setNationalGeoJSONLayer();
-        } else {
-            setMSAGeoJSONLayer();
-        }
+    function changeToggle(select, layer_options) {
+        select.clear();
+        select.clearOptions();
+        select.addOption(layer_options);
     }
 
-
-    function setTimeSeries() {
-        // TODO: more succinct way to do this?
-        var msaField = msaValToName($('#MSA_toggle')[0].selectize.items[0]);
-        // For the time being I will use the TCVIS.Config.ntdField var
-        // since the census data are not in the time series dataset yet
-        // TODO: connect this to the map var toggle 
-        if (msaField !== undefined && TCVIZ.Config.ntdField) {
-            TCVIZ.Config.sqlConnections.timeSeriesSql
-                .getJson(TCVIZ.Config.ntdField, msaField)
-                .done(function(data) {
-                    // TODO: Remove chart elements before adding more
-                    TCVIZ.Config.timeSeries.clear();
-                    TCVIZ.Config.timeSeries.plot(data, TCVIZ.Config.ntdField);
-                });
-        }
-    }
-
-
-    /**
-     * React to change in zoom level, changing map if user crosses threshold
-     * @return {[type]}
-     */
-    function onZoomChange() {
-        var startNationWide = isNationWide();
-        map.on('zoomend', function() {
-            if (startNationWide !== isNationWide()) {
-                if (isNationWide()) {
-                    changeToggle(layerToggle(), TCVIZ.Config.nationwide_layers);
-                    setDefaultMapDropdownValue();
-                    TCVIZ.Config.ntdField = layerToggleValue();
-                } else {
-                    changeToggle(layerToggle(), TCVIZ.Config.MSA_layers);
-                    setDefaultMapDropdownValue();
-                    TCVIZ.Config.censusField = layerToggleValue();
-                }
-            }
-        });
-    }
-
-    /**
-     * 
-     * @param  {[type]}
-     * @return {[type]}
-     */
-    function msaValToName(msaVal) {
-        return $('#MSA_toggle')[0].selectize.options[msaVal].text;
-    }
-    /** 
-     * Remove objects with an NA value for a particular key
-     * @param  {GeoJSON}
-     * @param  {String}
-     * @return {GeoJSON}
-     */
-    function removeNAs(geoJson, field) {
-        geoJson.features = _.filter(geoJson.features, function(d) {
-            return d.properties[field] != 'NA';
-        });
-        return geoJson;
-    }
 
     /**
      * Determine whether the app is zoomed out beyond nationwide
@@ -143,14 +67,57 @@ $(document).ready(function() {
     }
 
     /**
-     * Change layer selector options when zoom level changes
-     * @param  {Selector}
-     * @param  {Array}
+     * Initialize app
      */
-    function changeToggle(select, layer_options) {
-        select.clear();
-        select.clearOptions();
-        select.addOption(layer_options);
+    function initialize() {
+
+        /**
+         * All initialize functions
+         */
+        setUpEventListeners();
+        initializeDropdowns();
+        setMap();
+        setTimeSeries();
+
+        function initializeDropdowns() {
+            populateMapDropdown();
+            populateMSADropdown();
+            setMapDropdownValue();
+            setDefaultMSADropdownValue();
+        }
+
+        function populateMapDropdown() {
+            $('#toggle')[0].selectize
+                .addOption(TCVIZ.Config.nationwide_layers);
+        }
+
+        function populateMSADropdown() {
+            $('#MSA_toggle')[0].selectize.addOption(TCVIZ.Config.MSA_list);
+        }
+
+        function setDefaultMSADropdownValue() {
+            // TODO: what will the default value for MSA dropdown be?
+            $('#MSA_toggle')[0].selectize.setValue(TCVIZ.Config.defaultMSA);
+        }
+
+        function setUpEventListeners() {
+            setUpSidebarToggle();
+            setUpSelectize();
+        }
+
+        function setUpSelectize() {
+            $('.selectize').selectize({
+                create: true,
+                sortField: 'text'
+            });
+        }
+
+        function setUpSidebarToggle() {
+            $('#sidebar-toggle').on('click', function(el) {
+                el.preventDefault();
+                $('.sidebar').toggleClass('active');
+            });
+        }
     }
 
     /**
@@ -170,102 +137,143 @@ $(document).ready(function() {
     }
 
     /**
-     * Set the default/stored map variable value for the current 
-     * zoom level
+     * 
+     * @param  {[type]}
+     * @return {[type]}
      */
-    function setDefaultMapDropdownValue() {
+    function msaValToName(msaVal) {
+        return $('#MSA_toggle')[0].selectize.options[msaVal].text;
+    }
+
+    /**
+     * React to change in zoom level, changing map if user crosses threshold
+     * @return {[type]}
+     */
+    function onZoomChange() {
+        var startNationWide = isNationWide();
+        // move this ^ out to it's own
+        map.on('zoomend', function() {
+            if (startNationWide !== isNationWide()) {
+                if (isNationWide()) {
+                    changeToggle(layerToggle(), TCVIZ.Config.nationwide_layers);
+                    setMapDropdownValue();
+                    TCVIZ.State.ntdField = layerToggleValue();
+                } else {
+                    changeToggle(layerToggle(), TCVIZ.Config.MSA_layers);
+                    setMapDropdownValue();
+                    TCVIZ.State.censusField = layerToggleValue();
+                }
+            }
+        });
+    }
+
+    /** 
+     * Remove objects with an NA value for a particular key
+     * @param  {GeoJSON}
+     * @param  {String}
+     * @return {GeoJSON}
+     */
+    function removeNAs(geoJson, field) {
+        geoJson.features = _.filter(geoJson.features, function(d) {
+            return d.properties[field] != 'NA';
+        });
+        return geoJson;
+    }
+
+    /**
+     * Send a layer to the map calling function based on map zoom
+     */
+    function setMap() {
         if (isNationWide()) {
-            $('#toggle')[0].selectize.setValue(TCVIZ.Config.ntdField);
+            setNationalGeoJSONLayer();
         } else {
-            $('#toggle')[0].selectize.setValue(TCVIZ.Config.censusField);
+            setMSAGeoJSONLayer();
         }
     }
 
     /**
-     * Initialize app
+     * Set polygon layer of MSAs
      */
-    function initialize() {
-
-        /*
-        Event listeners
-         */
-
-        function setUpSelectize() {
-            $('.selectize').selectize({
-                create: true,
-                sortField: 'text'
-            });
+    function setMSAGeoJSONLayer() {
+        var valueField = layerToggleValue();
+        if (valueField !== undefined) {
+            if (map.hasLayer(TCVIZ.State.featureGroup)) {
+                map.removeLayer(TCVIZ.State.featureGroup);
+            }
+            TCVIZ.State.censusField = valueField;
+            // TCVIZ.Connections.tractMap.getJson()
+            //     .done(function(data) {
+            // // TODO: add the rest of this function once census data are up
+            //     });
         }
-
-        function setUpSidebarToggle() {
-            $('#sidebar-toggle').on('click', function(el) {
-                el.preventDefault();
-                $('.sidebar').toggleClass('active');
-            });
-        }
-
-        function setUpEventListeners() {
-            setUpSidebarToggle();
-            setUpSelectize();
-        }
-
-        /*
-        Dropdowns
-         */
-        function populateMapDropdown() {
-            $('#toggle')[0].selectize
-                .addOption(TCVIZ.Config.nationwide_layers);
-        }
-
-        function populateMSADropdown() {
-            $('#MSA_toggle')[0].selectize.addOption(TCVIZ.Config.MSA_list);
-        }
-
-        function setDefaultMSADropdownValue() {
-            // TODO: what will the default value for MSA dropdown be?
-            $('#MSA_toggle')[0].selectize.setValue(TCVIZ.Config.currentMSA);
-        }
-
-        function initializeDropdowns() {
-            populateMapDropdown();
-            populateMSADropdown();
-            setDefaultMapDropdownValue();
-            setDefaultMSADropdownValue();
-        }
-
-        /**
-         * All initialize functions
-         */
-        setUpEventListeners();
-        initializeDropdowns();
-        setMap();
-        setTimeSeries();
     }
 
-    /*
-     Initialize objects
-     TODO: Should these live somehere else?
+    /**
+     * Set a nationwide point layer
      */
-    // Carto connections
-    TCVIZ.Config.sqlConnections.msaMap = new TCVIZ.Carto.SQL('msaMap');
-    TCVIZ.Config.sqlConnections.timeSeriesSql = new TCVIZ.Carto.SQL('timeSeries');
-    TCVIZ.Config.sqlConnections.tractMap = new TCVIZ.Carto.SQL('censusTracts');
+    function setNationalGeoJSONLayer() {
+        var valueField = $('#toggle')[0].selectize.items[0];
+        if (valueField !== undefined) {
+            if (map.hasLayer(TCVIZ.State.featureGroup)) {
+                map.removeLayer(TCVIZ.State.featureGroup);
+            }
+            TCVIZ.State.ntdField = valueField;
+            TCVIZ.Connections.msaMap.getJson(valueField)
+                .done(function(data) {
+                    data = removeNAs(data, valueField);
+                    TCVIZ.State.featureGroup = L.geoJson(data, {
+                        pointToLayer: function(feature, latlng) {
+                            return new L.CircleMarker(latlng, {
+                                // * TODO: function to scale values
+                                // * TODO: timeseries stylesheet interferes with
+                                // *  graduated symbols, fix this
+                                radius: feature.properties[valueField] / 1000000,
+                                fillOpacity: 0.5,
+                                color: '#0000FF'
+                            });
+                        }
+                    });
+                    map.addLayer(TCVIZ.State.featureGroup);
+                });
+        }
+    }
 
-    // Time Series plot
-    TCVIZ.Config.timeSeries = new TCVIZ.Charts.TimeSeries();
+    function setTimeSeries() {
+        // TODO: more succinct way to do this?
+        var msaField = msaValToName($('#MSA_toggle')[0].selectize.items[0]);
+        // For the time being I will use the TCVIS.Config.ntdField var
+        // since the census data are not in the time series dataset yet
+        // TODO: connect this to the map var toggle 
+        if (msaField !== undefined && TCVIZ.State.ntdField !== null) {
+            TCVIZ.Connections.timeSeriesSql
+                .getJson(TCVIZ.State.ntdField, msaField)
+                .done(function(data) {
+                    // TODO: Remove chart elements before adding more
+                    TCVIZ.State.timeseries.clear();
+                    TCVIZ.State.timeseries.plot(data, TCVIZ.State.ntdField);
+                });
+        }
+    }
 
-    /*
-    Initialize
+    /**
+     * Set the default/stored map variable value for the current 
+     * zoom level
      */
-    initialize();
+    function setMapDropdownValue() {
+        if (isNationWide()) {
+            if (TCVIZ.State.ntdField !== undefined) {
+                $('#toggle')[0].selectize.setValue(TCVIZ.State.ntdField);
+            } else {
+                $('#toggle')[0].selectize.setValue(TCVIZ.Config.defaultNtdField);
+            }
 
-    /*
-    Listeners
-     */
-    $('#toggle')[0].selectize.on('change', function() {
-        setMap();
-        setTimeSeries();
-    });
-    $('#MSA_toggle')[0].selectize.on('change', setTimeSeries);
-    map.on('zoomstart', onZoomChange);
+        } else {
+            if (TCVIZ.State.censusField !== undefined) {
+                $('#toggle')[0].selectize.setValue(TCVIZ.State.censusField);
+            } else {
+                $('#toggle')[0].selectize.setValue(TCVIZ.Config.defaultCensusField);
+            }
+        }
+    }
+
 });

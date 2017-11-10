f8d430b63269831bb9fc7096783043c1b97b0710
CloudNiner <andrew@cloudniner.co>
2017-11-09 19:03:44 -0500

    Refactor map zoom/pan/state logic in main.js
    Significant refactor that attempts to clean up
    handling of all user events.
    
    Some improvements:
    - Remove MSA/Nationwide layer switching on zoom level
      - Now tied only to:
        - Change in MSA toggle
        - Clicks on popup links
      - Indirectly fixes #82 since the layer switch is no
        longer tied to the zoom change
      - Allows removal of a significant amount of code
    - Only remove the old layer immediately before adding
      the new one. Prevents race conditions where two
      layers could be added to the map
    - Fix issue where MSA selection wouldn't switch properly
      when already at the tract level (#71)
    - Refactors main.js map/toggle logic to tie state directly
      to the value set in the toggles, removing the global
      state that was unnecessarily duplicated since we can
      always just get the current layer/MSA state from the toggles
    - Refactors map layer add/remove methods to take msaId and
      layerId as arguments, rather than pulling them within the
      functions. Makes code paths clearer.
    - Updates setLegend calls so that the legend is updated only
      when the active layer is switched. Prevents new legend and
      layer from updating out of sync if layer load is slow.
    - Updates the TVIZ.State object to match the current set of
      vars that are in use elsewhere in app

--- app/assets/js/src/tcviz-state.js
+++ app/assets/js/src/tcviz-state.js
@@ -4,12 +4,5 @@ TCVIZ.State = {
      */
     ntdField: null,
     censusField: null,
-    currentMSA: null,
-    startNationWide: null,
-    // TODO: should the two objects below go in their own TCVIS objects?
-
-    /*
-    Time series object
-     */
-    timeSeries: null
+    sizeBreaks: null
 };
